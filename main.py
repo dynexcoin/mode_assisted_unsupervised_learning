89a8ae6e73d4efe9d64788939316900e3f572401
Dynex [DNX] <113523376+dynexcoin@users.noreply.github.com>
2023-09-20 09:48:58 +0200

    Add files via upload
    - Replaced exact solver with QUBO Sampler
    - Re-structured code for better readability

--- main.py
+++ main.py
@@ -1,188 +1,230 @@
+# Import libraries
 import numpy as np
 import copy
+import time
+from tqdm import tqdm
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="whitegrid")
+
+# Import custom modules
 from create_shifting_bars import * 
 from exactZ import *
 from randomDBM import *
 from get_data_expectation import *
 from get_model_expectation_CD import *
 from get_model_expectation_mode import *
-from get_model_expectation_CD import *
 
-# Clear workspace, set random seed
-#np.random.seed(42)
-
-# Define the size of the RBM
-nodes = [6, 6]
-
-# Generate some example data (replace with your actual data)
-data, p_data = create_shifting_bars(nodes[0], nodes[0]//2)
-print(data)
-
-# Size of RBM ensemble
-numEnsemble = 12
-# Number of CD iterations
-CDiters = 1
-# Total parameter updates
-iterations = 100000
-# Plot KL every Nth update
-plotPer = 1000
-# CD learning rate
-lr0 = 0.2
-# Parameters for sigmoidal mode probability
-alpha = 20
-beta = -6
-
-kls_cd_e = np.zeros((numEnsemble, iterations//plotPer))
-kls_mode_e = np.zeros((numEnsemble, iterations//plotPer))
-
-mode_probs = np.zeros((numEnsemble, 2**nodes[0]))
-cd_probs = np.zeros((numEnsemble, 2**nodes[0]))
-
-for j in range(numEnsemble):
-    # Initialize RBM randomly
-    print('numEnsemble =', j)
-    rbm_cd = randomDBM(nodes)
-    rbm_mode = copy.deepcopy(rbm_cd)
-
-    kls_cd = np.zeros((1,iterations//plotPer))
-    kls_mode = np.zeros((1,iterations//plotPer))
-
-    for i in range(iterations-1):
-        lr = lr0
-        # Sample (with replacement) from training set
-        datasamp = data[np.random.choice(data.shape[0], size=data.shape[0], replace=False)]
-        if i == 0 or (i + 1) % plotPer == 0:
-            if i == 0:
-                count = 0
-            else:
-                count = int((i+1)/plotPer)   
-
-            #print(j, i, count)
-            # Compute exact KL divergences of two RBMs
-
-            log_p_cd, _, _ = exactZ(rbm_cd)
-            log_p_mode, _, _ = exactZ(rbm_mode)
-
-            kl_cd = p_data * (np.log(p_data) - log_p_cd)
-            kl_cd = kl_cd[~np.isnan(kl_cd)]
-            kls_cd[0,count] = np.sum(kl_cd)
-
-            kl_mode = p_data * (np.log(p_data) - log_p_mode)
-            kl_mode = kl_mode[~np.isnan(kl_mode)]
-            kls_mode[0,count] = np.sum(kl_mode)
-
-        # Calculate CD update + update parameters
-
-        cd_vhd, cd_vd, cd_hd = get_data_expectation(rbm_cd, datasamp)
-        cd_vhm, cd_vm, cd_hm, _ = get_model_expectation_CD(rbm_cd, datasamp, CDiters)
-
-        dW = lr * (cd_vhd - cd_vhm)
-        dV = lr * (cd_vd - cd_vm)
-        dH = lr * (cd_hd - cd_hm)
-
-        rbm_cd['w'][0] += dW
-        rbm_cd['b'][0] += dV
-        rbm_cd['b'][1] += dH
-
-        # Calculate mode training update + update parameters
-        mode_vhd, mode_vd, mode_hd = get_data_expectation(rbm_mode, datasamp)
-
-        # Compute mode sample probability
-        sigm = 0.1 / (1 + np.exp(-alpha * (i+1) / iterations - beta))
-
-        # Perform mode or CD update  np.random.rand()
-        if np.random.rand() <= sigm:
-            mode_vhm, mode_vm, mode_hm, mode_push = get_model_expectation_mode(rbm_mode)
-            lr = lr * mode_push
-        else:
-            mode_vhm, mode_vm, mode_hm,_ = get_model_expectation_CD(rbm_mode, datasamp, CDiters)
-            lr = lr0
-
-        dW = lr * (mode_vhd - mode_vhm)
-        dV = lr * (mode_vd - mode_vm)
-        dH = lr * (mode_hd - mode_hm)
-
-        rbm_mode['w'][0] += dW
-        rbm_mode['b'][0] += dV
-        rbm_mode['b'][1] += dH
-
-    # Save KL divergences and probabilities for plotting.
-    kls_cd_e[j, :] = kls_cd
-    kls_mode_e[j, :] = kls_mode
-
-    log_p_cd, _, _ = exactZ(rbm_cd)
-    log_p_mode, _, _ = exactZ(rbm_mode)
-
-    cd_probs[j, :] = (np.log(p_data) - log_p_cd).T
-    mode_probs[j, :] = (np.log(p_data) - log_p_mode).T
+# CONSTANTS
+SEED = 42
+NODES = [6, 6]  # [Visible nodes, Hidden nodes]
+NUM_ENSEMBLES = 10  # Number of RBM ensembles
+CD_ITERS = 1  # Number of Contrastive Divergence iterations
+TOTAL_ITERATIONS = 20000  # Total parameter updates
+PLOT_INTERVAL = 200  # Plot KL divergence every Nth update
+INITIAL_LR = 0.2  # Initial learning rate for Contrastive Divergence
+ALPHA = 20  # Alpha for sigmoidal mode probability
+BETA = -6  # Beta for sigmoidal mode probability
+P_MAX = 0.1  # Maximum mode probability
 
 
-print('training end')
+# np.random.seed(SEED) # Set the random seed
+rbm_nodes = NODES # Define the size of the Restricted Boltzmann Machine (RBM)
+ensemble_count = NUM_ENSEMBLES # Number of ensembles in the RBM
+cd_iterations = CD_ITERS # Number of Contrastive Divergence iterations
+total_iterations = TOTAL_ITERATIONS # Total number of parameter updates during training
+plot_interval = PLOT_INTERVAL # Interval for plotting Kullback-Leibler divergence
+initial_learning_rate = INITIAL_LR # Initial learning rate for Contrastive Divergence algorithm
 
-# Produce plots
-plotXaxis = np.arange(0, iterations, plotPer)
-x = plotXaxis
-xx = x
-y11 = np.min(kls_cd_e, axis=0)
-y12 = np.max(kls_cd_e, axis=0)
-y21 = np.min(kls_mode_e, axis=0)
-y22 = np.max(kls_mode_e, axis=0)
-alpha = 0.5
-beta = 0.1
+# Parameters for calculating the sigmoidal mode probability
+sigmoid_alpha = ALPHA
+sigmoid_beta = BETA
+max_mode_probability = P_MAX
 
-# Define colors
-modeColor = [0.8500, 0.3250, 0.0980]
-cdColor = [0, 0.4470, 0.7410]
+# Parameters for Q_model_type and Q_solver_type
+Q_model_type = 'rbm' # Choose either 'rbm' or 'dbn'
+Q_solver_type = 'Sampler' # Choose either 'Exact' or 'Sampler'
 
-# Create new figure and subplots
-fig, axs = plt.subplots(2, 2, figsize=(12, 8))
+def calculate_exact_kl(p_data, rbm):
+    """
+    Calculate the exact Kullback-Leibler (KL) divergence between the data distribution and RBM's model distribution.
+    Args:
+    - p_data (array): Data probability distribution
+    - rbm (dict): RBM model
+    
+    Returns:
+    - float: The calculated KL divergence
+    """
+    # Compute the partition function and probabilities for the model
+    log_p, _, _ = exactZ(rbm)
+    kl = p_data * (np.log(p_data) - log_p)
+    kl = kl[~np.isnan(kl)]
+    return np.sum(kl)
 
-# Plot for subplot (2,2,1)
-aa = np.median(kls_cd_e.T, axis=1)
-axs[0, 0].plot(plotXaxis, aa, linewidth=2.5, color=cdColor)
-axs[0, 0].fill_between(xx, y11,y12, color=cdColor, alpha=0.2)
-axs[0, 0].set_title('CD')
-axs[0, 0].set_xlabel('Iterations', fontweight='bold')
-axs[0, 0].set_ylabel('KL Divergence', fontweight='bold')
-axs[0, 0].grid(True, which='both', linestyle=':')
-axs[0, 0].set_ylim([0, 2])
-axs[0, 0].set_xlim([0, iterations])
+def update_parameters(rbm, data_sample, expectation, lr):
+    """
+    Update the parameters of the RBM based on data and model expectations.
+    Args:
+    - rbm (dict): The RBM model
+    - data_sample (array): The data sample
+    - expectation (array): Model expectation values
+    - lr (float): Learning rate
+    
+    Returns:
+    - dict: Updated RBM model
+    """
+    # Compute expectations from data and model
+    vhd, vd, hd = get_data_expectation(rbm, data_sample)
+    vhm, vm, hm, push_factor = expectation
 
+    # modify learning rate based on mode push_factor
+    lr = lr * push_factor if push_factor else lr
 
-# Plot for subplot (2,2,2)
-bb = np.median(kls_mode_e.T, axis=1)
-axs[0, 1].plot(plotXaxis, bb, '-', linewidth=2.5, color=modeColor)
-axs[0, 1].fill_between(xx, y21,y22, color=modeColor, alpha=0.2)
-axs[0, 1].set_xlabel('Iterations', fontweight='bold')
-axs[0, 1].set_ylabel('KL Divergence', fontweight='bold', color='black')
-axs[0, 1].set_title('Mode Training')
-axs[0, 1].set_ylim([0, 2])
-axs[0, 1].grid(True, which='both', linestyle=':')
-cc = 0.1 / (1 + np.exp(-alpha * plotXaxis / iterations - beta))
-axs[0, 1].plot(plotXaxis, cc, '--', linewidth=1.0, color=[0.8500, 0.3250, 0.0980, 0.75])
-axs[0, 1].twinx().set_ylabel(r'$P_{\rm{mode}}$', color='black')
+    # Perform parameter updates
+    dW = lr * (vhd - vhm)
+    dV = lr * (vd - vm)
+    dH = lr * (hd - hm)
+    rbm['w'][0] += dW
+    rbm['b'][0] += dV
+    rbm['b'][1] += dH
 
-# Plot for subplot (2,2,3)
-axs[1, 0].bar(range(cd_probs.shape[1]), np.median(cd_probs.T, axis=1), color=cdColor)
-axs[1, 0].set_xlabel('Visible Configuration (Index)', fontweight='bold')
-axs[1, 0].set_ylabel('Log Probability', fontweight='bold')
-axs[1, 0].grid(True, which='both', linestyle=':')
-axs[1, 0].legend([r'$\log\left(\frac{q_i}{p_i}\right)$'], loc='upper left')
-axs[1, 0].set_ylim([-1, 1])
+    return rbm
+    
+def plot_results(plotXaxis, kl_cd_ensemble, kl_mode_ensemble, cd_probs, mode_probs, sigm_values):
+    """
+    Plot the training and evaluation results.
+    """
+    # colors = sns.color_palette(["#3070b7", "#c95c2e"]) #Original colors from the paper
+    colors = sns.color_palette("deep")
+    cdColor = colors[0]
+    modeColor = colors[1]
+    
+    # Create subplots
+    fig, axs = plt.subplots(3, 2, figsize=(12, 12)) 
 
-# Plot for subplot (2,2,4)
-axs[1, 1].bar(range(mode_probs.shape[1]) , np.median(mode_probs.T, axis=1), color=modeColor)
-axs[1, 1].set_xlabel('Visible Configuration (Index)', fontweight='bold')
-axs[1, 1].set_ylabel('Log Probability', fontweight='bold')
-axs[1, 1].grid(True, which='both', linestyle=':')
-axs[1, 1].legend([r'$\log\left(\frac{q_i}{p_i}\right)$'], loc='upper left')
-axs[1, 1].set_ylim([-1, 1])
+    # Subplot 1: CD training
+    plot_single_subplot(axs[0, 0], plotXaxis, kl_cd_ensemble, 'CD', 'Iterations', 'KL Divergence', cdColor, logscale=False)
 
+    # Subplot 2: Mode training
+    plot_single_subplot(axs[0, 1], plotXaxis, kl_mode_ensemble, 'Mode Training', 'Iterations', 'KL Divergence', modeColor, logscale=False)
 
+    # Add sigmoid curve to subplot 2
+    ax2 = axs[0, 1].twinx()
+    ax2.plot(plotXaxis, sigm_values.flatten(), ':', linewidth=1.0, color=modeColor)
+    ax2.set_ylim([np.min(sigm_values), np.max(sigm_values)])
+    ax2.set_ylabel(r'$P_{\rm{mode}}$', fontsize=14, color='black')
+    ax2.grid(False)  # Disable the grid for the secondary y-axis
 
-# Fine-tune the layout
-plt.tight_layout()
+    # Subplot 3: CD training (log scale)
+    plot_single_subplot(axs[1, 0], plotXaxis, kl_cd_ensemble, 'CD (Log scale)', 'Iterations', 'KL Divergence', cdColor, logscale=True)
 
-# Save the figure or show it
-plt.show()
+    # Subplot 4: Mode training (log scale)
+    plot_single_subplot(axs[1, 1], plotXaxis, kl_mode_ensemble, 'Mode Training (Log scale)', 'Iterations', 'KL Divergence', modeColor, logscale=True)
+
+    # Subplot 5: Final probabilities for CD training
+    plot_prob_subplot(axs[2, 0], cd_probs, 'Visible Configuration (Index)', 'Log Probability', cdColor)
+
+    # Subplot 6: Final probabilities for Mode training
+    plot_prob_subplot(axs[2, 1], mode_probs, 'Visible Configuration (Index)', 'Log Probability', modeColor)
+
+    plt.tight_layout()
+    plt.show()
+    
+def plot_single_subplot(ax, x, ensemble, title, xlabel, ylabel, color, logscale):
+    """Utility function to plot a single subplot."""
+    y = np.median(ensemble.T, axis=1)
+    y_min, y_max = np.min(ensemble, axis=0), np.max(ensemble, axis=0)
+    
+    ax.plot(x, y, linewidth=2.5, color=color)
+    ax.fill_between(x, y_min, y_max, color=color, alpha=0.2)
+    ax.set_title(title, fontsize=16, fontweight='bold')
+    ax.set_xlabel(xlabel, fontsize=14, fontweight='bold')
+    ax.set_ylabel(ylabel, fontsize=14, fontweight='bold')
+    ax.grid(True, linestyle='--')
+    ax.set_ylim([0.01, 2] if logscale else [0, 2])
+    ax.set_xlim([0, total_iterations])
+    if logscale:
+        ax.set_yscale('log')
+
+def plot_prob_subplot(ax, probs, xlabel, ylabel, color):
+    """Utility function to plot final probabilities."""
+    ax.bar(range(probs.shape[1]), np.median(probs, axis=0), color=color)
+    ax.set_xlabel(xlabel, fontsize=14, fontweight='bold')
+    ax.set_ylabel(ylabel, fontsize=14, fontweight='bold')
+    ax.grid(True, linestyle='--')
+    ax.legend([r'$\log\left(\frac{q_i}{p_i}\right)$'], fontsize=12, loc='upper left')
+    ax.set_ylim([-1, 1])
+
+def main():
+    start_time = time.time()
+    
+    # Generate example data with shifting bars, note that you can replace this with your actual data
+    data, p_data = create_shifting_bars(rbm_nodes[0], rbm_nodes[0]//2)
+    
+    # Initialize arrays to store KL divergence values for multiple ensembles
+    kl_cd_ensemble = np.zeros((ensemble_count, total_iterations//plot_interval))
+    kl_mode_ensemble = np.zeros((ensemble_count, total_iterations//plot_interval))
+    
+    # Initialize arrays to store probabilities calculated from different training approaches (CD and Mode)
+    cd_probs = np.zeros((ensemble_count, 2**rbm_nodes[0]))
+    mode_probs = np.zeros((ensemble_count, 2**rbm_nodes[0]))
+
+    # Loop over multiple ensembles
+    for ensemble_idx in range(ensemble_count):
+        print()
+        
+        # Initialize RBM randomly for both training approaches (CD and Mode)
+        rbm_cd = randomDBM(rbm_nodes)
+        rbm_mode = copy.deepcopy(rbm_cd)
+
+        # Initialize arrays to store KL divergence and sigmoid values for plotting
+        kl_cd_values = np.zeros((1, total_iterations//plot_interval))
+        kl_mode_values = np.zeros((1, total_iterations//plot_interval))
+        sigm_values  = np.zeros((1, total_iterations//plot_interval))
+
+        # Main training loop
+        for iter_idx in tqdm(range(total_iterations), desc=f"Ensemble iteration {ensemble_idx+1}/{ensemble_count} Training Progress", ncols=100):
+            # Set the learning rate, can implement learning rate decay here
+            learning_rate = initial_learning_rate
+            
+            # Choose a random subset of data for this iteration
+            data_sample = data[np.random.choice(data.shape[0], size=data.shape[0], replace=False)]
+
+            # Compute and store KL divergence at specific intervals for plotting
+            if iter_idx == 0 or (iter_idx) % plot_interval == 0:
+                count = (iter_idx) // plot_interval if iter_idx != 0 else 0
+
+                kl_cd_values[0, count] = calculate_exact_kl(p_data, rbm_cd)
+                kl_mode_values[0, count] = calculate_exact_kl(p_data, rbm_mode)
+            
+            # Update rbm_cd using Contrastive Divergence (CD)
+            cd_expectation = get_model_expectation_CD(rbm_cd, data_sample, cd_iterations)
+            rbm_cd = update_parameters(rbm_cd, data_sample, cd_expectation, learning_rate)
+
+            # Compute mode sample probability based on sigmoid function
+            sigm = max_mode_probability / (1 + np.exp(-sigmoid_alpha * (iter_idx) / total_iterations - sigmoid_beta))
+            sigm_values[0, count] = sigm
+            
+            # Update rbm_mode using either Contrastive Divergence (CD) or "Mode" based on the probability
+            mode_expectation = get_model_expectation_mode(rbm_mode, Q_model_type, Q_solver_type) if np.random.rand() <= sigm else get_model_expectation_CD(rbm_mode, data_sample, cd_iterations)
+            rbm_mode = update_parameters(rbm_mode, data_sample, mode_expectation, learning_rate)
+        
+        # Store the computed KL values for each ensemble
+        kl_cd_ensemble[ensemble_idx, :] = kl_cd_values
+        kl_mode_ensemble[ensemble_idx, :] = kl_mode_values
+
+        # Compute the final probabilities for each model
+        log_p_cd, _, _ = exactZ(rbm_cd)
+        log_p_mode, _, _ = exactZ(rbm_mode)
+        
+        # Store the final probabilities for each ensemble and method
+        cd_probs[ensemble_idx, :] = (np.log(p_data) - log_p_cd).T
+        mode_probs[ensemble_idx, :] = (np.log(p_data) - log_p_mode).T
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f'Training End, took {elapsed_time:.2f} secs')
+    plotXaxis = np.arange(0, total_iterations, plot_interval)
+    plot_results(plotXaxis, kl_cd_ensemble, kl_mode_ensemble, cd_probs, mode_probs, sigm_values)
+
+if __name__ == "__main__":
+    main()
